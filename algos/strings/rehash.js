d10b96e2569ad3ac756d7168380785cd0b52d9b1
Max R <max.l.rauchman@gmail.com>
2023-04-26 10:58:40 -0400

    2nd Test Case

--- algos/strings/rehash.js
+++ algos/strings/rehash.js
@@ -5,6 +5,10 @@ Given by Riot games.
 const str1 = "b70a164c32a20c10";
 const expected1 = "a184b70c42";
 
+const str2 = "b70a164c32a20c10a20";
+const expected2 = "a204b70c42";
+
+
 /**
  * Rehashes an incorrectly hashed string by combining letter count occurrences
  * and alphabetizing them.
